0c7d4825970c74041968ff9466e29cfb6597d285
liutao <tao.liu@uurobot.cn>
2018-09-12 17:03:18 +0800

    shapeButton

---
+++ shapeButton/src/main/java/com/blue/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.blue.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.blue.R
+import com.blue.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
